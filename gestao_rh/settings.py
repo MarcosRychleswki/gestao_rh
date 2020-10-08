d9cdf0e1f34530f03569b30d8bcc12e67959f31f
MarcosRychlewski <vinirychlewski@gmail.com>
2020-10-08 11:27:58 -0300

    Criando a app funcionarios

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.empresa',
+    'apps.empresas',
+    'apps.funcionarios',
+
 ]
 
 MIDDLEWARE = [
