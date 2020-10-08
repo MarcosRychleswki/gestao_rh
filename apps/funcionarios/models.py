049d3bfd8026e6697438b64ed246091cd2c432c6
MarcosRychlewski <vinirychlewski@gmail.com>
2020-10-08 14:11:28 -0300

    database interface

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+# on_delete=models.PROTECT ( serve para quando o user for deletado ele deletar na class Funcionario )
+from django.contrib.auth.models import User
+#user pode estar em multiplos departamentos
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
+
+
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
